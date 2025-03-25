bb83c9a1841990d7e9010bdec264d0a0f12d577a
brother <hacker@gmail.com>
2025-03-25 14:00:50 +0300

    сделал

---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+import asyncio
+from dotenv import load_dotenv, find_dotenv
+from loguru import logger
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+import requests
+from bs4 import BeautifulSoup
+from random import choice
+
+
+load_dotenv(find_dotenv())
+TOKEN = os.getenv("TOKEN")
+CHANNAL_ID = -1002609878025
+
+
+async def main():
+    logger.add("file.log",
+               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+               rotation="3 days",
+               backtrace=True,
+               diagnose=True)
+
+    bot = Bot(token=TOKEN)
+    logger.info("Бот создан")
+    dp = Dispatcher()
+    logger.info("Диспетчер создан")
+
+    async def send_random_joke():
+        while True:
+            try:
+                response = requests.get('https://www.anekdot.ru/random/anekdot/')
+                if response.status_code == 200:
+                    soup = BeautifulSoup(response.text, 'html.parser')
+                    jokes = soup.find_all('div', class_='text')
+
+                    random_joke = choice(jokes).text.strip()
+                    anekdot = random_joke
+                else:
+                    anekdot = "Не удалось получить анекдот"
+
+                await bot.send_message(CHANNAL_ID, f"Анекдот: {anekdot}")
+                logger.info(f"Опублекован анекдот: {anekdot}")
+            except Exception as e:
+                logger.error(f"Ошибка при отправке сообщения {e}")
+
+            await asyncio.sleep(3600)
+
+    @dp.message(Command("start"))
+    async def send_welcome(message: types.Message):
+        await message.answer("Бот запущен! Он будет отправлять анекдоты!")
+        logger.info("Бот запущен")
+
+    task = asyncio.create_task(send_random_joke())
+
+    try:
+        await dp.start_polling(bot)
+    finally:
+        task.cancel()
+        await bot.session.close()
+        logger.info("Бот остановлен")
+
+if __name__ == '__main__':
+    asyncio.run(main())
